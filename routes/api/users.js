48bce5ed908580ddad719f3dae811e32c6de55f7
tmartinez114 <tmartinez114@gmail.com>
2019-10-17 01:36:02 -0400

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -33,7 +34,9 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // Get users gravatar
@@ -51,10 +54,16 @@ router.post(
       });
 
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      // Hash the password
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
 
       // Return jsonwebtoken
 
-      res.send('User route');
+      res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
